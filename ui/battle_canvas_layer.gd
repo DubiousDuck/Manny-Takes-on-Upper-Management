915f717b6d25e6c99664e5242dc9e4178655c2b8
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-02-12 16:19:44 -0800

    Battle menu turned into general pause menu

--- ui/battle_canvas_layer.gd
+++ ui/battle_canvas_layer.gd
@@ -3,11 +3,17 @@ extends CanvasLayer
 var menuIsDisplayed: bool = false
 @onready var battle_menu_control = $BattleMenuControl
 @onready var label = $BattleMenuControl/Label
+@onready var exit_level = $BattleMenuControl/HBoxContainer/ExitLevel
+@onready var restart_level = $BattleMenuControl/HBoxContainer/RestartLevel
+
+@export var isBattleScene: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	updateLabelText()
+	if !isBattleScene:
+		exit_level.visible = false
+		restart_level.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,24 +24,21 @@ func _input(event):
 		menuIsDisplayed = !menuIsDisplayed
 		battle_menu_control.visible = menuIsDisplayed
 		
-		var filename = get_tree().current_scene.scene_file_path.get_file().replace(".tscn", "")
-		var parts = filename.split("_")  # Split the string by underscores
-		var i = 0
-		for word in parts:
-			parts[i] = word.capitalize()
-			i += 1
-		var result = " ".join(parts)      # Join the parts with spaces
-		
-		label.text = result
-		
-		
 		if menuIsDisplayed:
 			get_tree().paused = true
 		else:
 			get_tree().paused = false
-		
-		print("Escape key pressed! menu displayed = " + str(menuIsDisplayed))
 
+func updateLabelText():
+	var filename = get_tree().current_scene.scene_file_path.get_file().replace(".tscn", "")
+	var parts = filename.split("_")  # Split the string by underscores
+	var i = 0
+	for word in parts: # Iterates through, capitalizing each word
+		parts[i] = word.capitalize()
+		i += 1
+	var result = " ".join(parts)      # Join the parts with spaces
+	
+	label.text = result
 
 func _on_restart_level_pressed():
 	get_tree().paused = false
@@ -49,3 +52,9 @@ func _on_exit_level_pressed():
 
 func _on_quit_game_pressed():
 	get_tree().quit()
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	menuIsDisplayed = false
+	battle_menu_control.visible = menuIsDisplayed
