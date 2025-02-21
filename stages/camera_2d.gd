b70aa01f19f00cc137345618b0a017b8777c3427
clarkipeng <clarkipeng@gmail.com>
2025-02-20 20:32:12 -0800

    fix camera debug

--- stages/camera_2d.gd
+++ stages/camera_2d.gd
@@ -3,11 +3,3 @@ extends Camera2D
 func _ready():
 	Global.camera_top = $top_margin.global_position.y
 	Global.camera_low = $low_margin.global_position.y
-
-func _process(delta: float) -> void:
-	var player = get_parent().get_node("Player")
-	#print("offset",offset)
-	if not (player == null):
-		transform =player.position
-		offset = player.position
-	#print("offset",offset)
