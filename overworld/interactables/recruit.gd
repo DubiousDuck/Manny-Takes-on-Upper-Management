32c87bdb46509dc75a144641df98742ce153f704
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-02-26 20:29:50 -0800

    started to add the recruit npc

---
+++ overworld/interactables/recruit.gd
@@ -0,0 +1,17 @@
+extends Interactable
+
+class_name Recruit
+
+@export_file("*.tscn") var scene_to_go
+@export var locked=false
+
+func _interact_call_back():
+	if locked:
+		Global.start_dialogue(["this door is locked"])
+		await EventBus.ui_element_ended
+	else:
+		Global.start_dialogue(["test", "hello", "test again"])
+		await EventBus.ui_element_ended
+		Global.start_battle(self.name, [load("res://unit/params/healer.tres")])
+		get_tree().change_scene_to_file(scene_to_go)
+		
