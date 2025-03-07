cd0acd87bb20076a937d493f27dcbdd60f80a8ed
johncast123 <johnsebastiantan@gmail.com>
2025-03-07 15:17:00 -0800

    Added theme to Main Menu nodes, linked load to current load scene

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -3,3 +3,6 @@ extends CanvasLayer
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://overworld/sample_overworld.tscn")
+
+func _on_load_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/save_and_load/load_ui.tscn")
