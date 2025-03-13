53f2d0600fa48c473bd72e7b30a097061a23db21
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-03-12 20:51:04 -0700

    After recruiting, babies are automatically added if there is room

--- overworld/draft_scene/draft_scene.gd
+++ overworld/draft_scene/draft_scene.gd
@@ -16,25 +16,33 @@ const MEMBER = preload("res://overworld/party_comp/draggable_member.tscn")
 func back_to_overworld():
 	get_tree().change_scene_to_packed(Global.get_last_overworld_scene())
 
+func party_add_logic(ally_type):
+	if Global.max_party_num > Global.current_party.size():
+		print("adding to party")
+		Global.current_party.append(ally_type)
+	else:
+		print("adding to reserves")
+		Global.reserves.append(ally_type)
+
 func _on_mage_pressed():
 	var mage = preload("res://unit/params/mage.tres")
-	Global.reserves.append(mage)
+	party_add_logic(mage)
 	back_to_overworld()
 
 
 func _on_fighter_pressed():
 	var fighter = preload("res://unit/params/fighter.tres")
-	Global.reserves.append(fighter)
+	party_add_logic(fighter)
 	back_to_overworld()
 
 
 func _on_ranger_pressed():
 	var ranger = preload("res://unit/params/ranger.tres")
-	Global.reserves.append(ranger)
+	party_add_logic(ranger)
 	back_to_overworld()
 
 
 func _on_healer_pressed():
 	var healer = preload("res://unit/params/healer.tres")
-	Global.reserves.append(healer)
+	party_add_logic(healer)
 	back_to_overworld()
